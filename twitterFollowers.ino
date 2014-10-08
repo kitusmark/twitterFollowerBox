c08d194d815c8cf4cfd4b9e308fd5fcc6d41b344
kitusmark <electronicslab@icloud.com>
2014-10-09 00:02:03 +0200

    just updates

--- twitterFollowers.ino
+++ twitterFollowers.ino
@@ -11,31 +11,36 @@
   
   created 8 oct 2014
   Marc Cobler
-*/s
-
+*/
 #include <SPI.h>
 #include <Ethernet.h>
 
 //Assign a MAC address and a IP. Check this if system not working properly
 byte macAddress[] = {
   0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x01};
-IPAddress ip(192,168,1,111);
+IPAddress ip(192,168,1,22);
 
 //Create an EthernetClient object
 EthernetClient server;
-//delay between server requests, 60 seconds.
-const unsigned long requestInterval = 60000; 
+//delay between server requests, X seconds.
+const unsigned long requestInterval = 20000; 
 //Set the server name and store it in flash, char array. URL
-const char serverName[] PROGMEM= "jfmc.synology.me";
+const char serverName[] = "http://jfmc.synology.me";
 
 unsigned long lastAttemptTime = 0; //Last time connected to the server in milliseconds
 String buffer = "";                //String to read from the html info from the server
 String followers = "";            //String to hold the important data
 boolean readingFollowers = false; //If we are currently reading the twitter followers data
+String serverDirection = "";
 //======================================================================================================
 void setup() {
   //Convert the char Array of serverName to a String
-  charArrayToString();*************
+  unsigned int i;
+  for(i=0; i<= 22; i++)
+    {
+      serverDirection += serverName[i];
+    }
+ 
   // we reserve some space in memory in order to manipulate the strings
   buffer.reserve(100);
   followers.reserve(10); 
@@ -45,7 +50,7 @@ void setup() {
   while (!Serial) {
    ; //Simply do nothing 
   }
-
+  Serial.println(serverDirection);
   //We try some DHCP connection
   Serial.println("trying to get an IP using DHCP:");
   if (!Ethernet.begin(macAddress)) {
@@ -53,7 +58,7 @@ void setup() {
       Ethernet.begin(macAddress, ip);
   }
   Serial.println("Good! we got a valid address: ");
-  Serial.print(Ethernet.localIP());
+  Serial.println(Ethernet.localIP());
   
   //Connect to the server
   connectToServer();
@@ -83,7 +88,7 @@ void loop() {
       }
       // Once we know we're reading the followers, we save the number
       if (readingFollowers) {
-        if (inChar != '<' | inChar != '>') {
+        if ((inChar != '<') | (inChar != '>')) {
           followers += inChar;
         } 
         else {
@@ -91,7 +96,7 @@ void loop() {
           // you've reached the end of the facebo:
           readingFollowers = false;
           Serial.println("You're followed by: ");
-          Serial.print(followers + "people"); 
+          Serial.println(followers + "people"); 
           // close the connection to the server:
           server.stop(); 
         }
@@ -109,22 +114,16 @@ void loop() {
 void connectToServer() {
   //connecting to the server via Internet
   Serial.println("Connecting to the specified Server...");
-  String host = "";
   
   if(server.connect(serverName,80)) {
      Serial.println("Making the HTTP request...");
      //HTTP GET request to the specified server
      server.println("GET /arduinoFollowersBox/arduinoFollowersBox.php HTTP/1.1"); 
-     host.concat("HOST: ", serverName);
-     server.println(host);
+     server.println("HOST: " + serverDirection);
      server.println("Connection: close");
      server.println();
   }
-  
+  Serial.println("Success");
   // note the time of this connect attempt:
   lastAttemptTime = millis();
-}
-
-charArrayToString() {
-  
 }
